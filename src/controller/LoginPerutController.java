c3d4be71506adcef457a5454c9eeaeaa3da778c3
Fauzi Achmad Haruna <fauziharuna@gmail.com>
2018-05-03 11:12:16 +0700

    Perbaikan fungsi-fungsi interface dan UI components

---
+++ src/controller/LoginPerutController.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author fauziachmadharuna
+ */
+public class LoginPerutController implements Initializable {
+    private String username="admin";
+    private String password="admin";
+
+
+    @FXML
+    private Label statustext;
+
+    @FXML
+    private JFXTextField usernametext;
+
+    @FXML
+    private JFXPasswordField passtext;
+
+    @FXML
+    private JFXButton btlbtn;
+    @FXML
+    private AnchorPane bg1;
+    @FXML
+    private JFXButton loginbtn;
+    @FXML
+    private Label versi;
+
+
+    @FXML
+    public void Login(ActionEvent event) throws Exception {
+      
+       // if (usernametext.equals(username) && passtext.equals(password)) {
+            statustext.setText("Login Sukses");
+            //agar jendela login tertutup setelah berhasil login
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+
+            //memanggil jendela menu admins
+//            FXMLLoader loader = new FXMLLoader();
+//            loader.setLocation(getClass().getResource("/view/Menu.fxml"));
+//            loader.load();
+      try{
+//            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Menu.fxml"));
+//            Parent root1 = (Parent) fxmlLoader.load();
+//            Stage stage = new Stage();
+//            stage.initModality(Modality.APPLICATION_MODAL);
+//            stage.initStyle(StageStyle.UNDECORATED);
+//            stage.setScene(new Scene(root1));  
+//            stage.show();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/view/Menu.fxml"));
+            loader.load();
+            MenuController aController = loader.getController();
+//            aController.setUserName(loggedIn_user);
+            Parent parent = loader.getRoot();
+//            aController.btnHomeOnClick(event);
+            Stage primaryStage = new Stage();
+            Scene scene = new Scene(parent);
+            primaryStage.setScene(scene);
+//            primaryStage.setMaximized(true);
+            primaryStage.setTitle("Menu Admin");
+            primaryStage.show();
+          }catch(Exception ex){
+              ex.printStackTrace();
+              System.out.println("Error");
+          }
+            
+        //} else {
+         //   statustext.setText("akun atau sandi salah");
+       // }
+    }
+
+    @FXML
+    private void tutupBtn(ActionEvent event) {
+        Stage stage = (Stage) btlbtn.getScene().getWindow();
+        stage.close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -8,22 +8,33 @@ package controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
 
 /**
  * FXML Controller class
@@ -32,160 +43,153 @@ import javafx.stage.StageStyle;
  */
 public class MenuController implements Initializable {
 
-    @FXML
-    private JFXTextField tf_search;
-    @FXML
-    private Label label;
-    @FXML
-    private Pane pane_home;
-    @FXML
-    private JFXComboBox<?> combobox_bulan;
-    @FXML
     private Pane pane_transaksi;
+    private AnchorPane anchorPane_Pemasukan;
+    private AnchorPane anchorPane_pengeluaran;
+    AnchorPane home, pemasukan, pengeluaran, data_warga, laporan, pengaturan_iuran;
     @FXML
-    private JFXTextField tf_nominalIuran;
+    private StackPane sp_pane_holder;
     @FXML
-    private JFXTextField tf_jumlahKurang;
+    private Label lbl_exit;
     @FXML
-    private JFXTextField tf_status;
+    private Pane Admin;
     @FXML
-    private JFXButton btn_regWarga;
+    private MenuButton menuButton_user;
+    @FXML
+    private MenuItem item_admin;
+    @FXML
+    private MenuItem item_operator;
+    @FXML
+    private ImageView avatar_user;
+    @FXML
+    private Pane pane_navigation;
     @FXML
     private JFXButton btn_beranda;
-    @FXML
-    private JFXButton btn_transaksi;
-    @FXML
-    private JFXButton btn_laporan;
-    @FXML
-    private Pane pane_dataWarga;
-    @FXML
-    private JFXTextField tf_birthDate;
-    @FXML
-    private JFXTextField tf_username;
-    @FXML
-    private JFXTextField tf_address;
-    @FXML
-    private JFXTextField tf_identity;
-    @FXML
-    private JFXTextField tf_name;
-    @FXML
-    private JFXComboBox<?> cb_usertType;
-    @FXML
-    private JFXTextField tf_saldo;
-    @FXML
-    private JFXButton btn_save;
-    @FXML
-    private JFXButton btn_reset;
-    @FXML
-    private Label btn_exit;
-    @FXML
-    private JFXButton btn_edit;
-    @FXML
-    private JFXButton btn_delete;
-    @FXML
-    private JFXButton btn_add;
-    @FXML
-    private TableView<?> table_userName;
-    @FXML
-    private AnchorPane anchorPane_Pemasukan;
-    @FXML
-    private JFXButton btn_pengeluaran1;
-    @FXML
-    private JFXButton btn_pemasukan1;
-    @FXML
-    private AnchorPane anchorPane_pengeluaran;
-    @FXML
-    private JFXComboBox<?> cb_pilhPengeluaran;
-    @FXML
-    private JFXTextField tf_nominal_pengeluaran;
     @FXML
     private JFXButton btn_pemasukan;
     @FXML
     private JFXButton btn_pengeluaran;
+    @FXML
+    private JFXButton btn_regWarga;
+    @FXML
+    private JFXButton btn_laporan;
+    @FXML
+    private JFXButton btn_pengaturan_iuran;
+    @FXML
+    private JFXButton btn_website;
+    @FXML
+    private AnchorPane HolderPane;
+     
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        pane_transaksi.setVisible(false);
-        pane_home.setVisible(true);
-        pane_dataWarga.setVisible(false);
+        
+
+//        pane_transaksi.setVisible(false);
+//        pane_home.setVisible(true);
+//        pane_dataWarga.setVisible(false);
         // TODO
-    }    
+//        MenuItem option1=new MenuItem("Admin");
+//        MenuItem option2=new MenuItem("Operator");
+//       try {
+//            home = FXMLLoader.load(getClass().getResource("/view/Beranda.fxml"));
+//            pemasukan = FXMLLoader.load(getClass().getResource("/view/TransaksiPemasukan.fxml"));
+//            pengeluaran = FXMLLoader.load(getClass().getResource("/view/TransaksiPengeluaran.fxml"));
+//            data_warga = FXMLLoader.load(getClass().getResource("/view/RegistrasiWarga.fxml"));
+//            laporan = FXMLLoader.load(getClass().getResource("/view/Laporan.fxml"));
+//            pengaturan_iuran = FXMLLoader.load(getClass().getResource("/view/Pengaturan_iuran.fxml"));
+//            setNode(home);
+//       } catch (IOException ex) {
+//          Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+
+//    private void setNode(Node node) {
+//        pane_holder.getChildren().clear();
+//        pane_holder.getChildren().add((Node) node);
+//
+//        FadeTransition ft = new FadeTransition(Duration.millis(1000));
+//        ft.setNode(node);
+//        ft.setFromValue(0.1);
+//        ft.setToValue(1);
+//        ft.setCycleCount(1);
+//        ft.setAutoReverse(false);
+//        ft.play();
+
+
+
+    }
+
 
     @FXML
-    private void handleButtonAction(MouseEvent event) {
+    private void regisWarga(ActionEvent event) throws Exception{
+       FXMLLoader loader = new FXMLLoader();
+       loader.setLocation(getClass().getResource("/view/RegistrasiWarga.fxml"));
+       loader.load();
+       AnchorPane acBeranda=loader.getRoot();
+       sp_pane_holder.getChildren().clear();
+       sp_pane_holder.getChildren().add(acBeranda);
+
     }
 
     @FXML
-    private void regisWarga(ActionEvent event) {
-        pane_home.setVisible(false);
-        pane_dataWarga.setVisible(true);
-        pane_transaksi.setVisible(false);
-//         try{
-//            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/MenuRegistrasiWarga.fxml"));
-//            Parent root1 = (Parent) fxmlLoader.load();
-//            Stage stage = new Stage();
-//            stage.initModality(Modality.APPLICATION_MODAL);
-//            stage.initStyle(StageStyle.UNDECORATED);
-//            stage.setScene(new Scene(root1));  
-//            stage.show();
-//          }catch(Exception ex){
-//              ex.printStackTrace();
-//              System.out.println("Error");
-//          }
-       
+    private void beranda(ActionEvent event) throws Exception{
+        FXMLLoader loader=new FXMLLoader();
+        loader.setLocation(getClass().getResource("/view/Beranda.fxml"));
+//        HolderPane = 
+        loader.load();
+        AnchorPane root = loader.getRoot();
+        sp_pane_holder.getChildren().clear();
+        sp_pane_holder.getChildren().add(root);
+    }
+
+
+
+    @FXML
+    private void operatorItem(ActionEvent event) {
     }
 
     @FXML
-    private void beranda(ActionEvent event) {
-        pane_home.setVisible(true);
-        pane_transaksi.setVisible(false);
-        pane_dataWarga.setVisible(false);
+    private void transaksiPemasukan(ActionEvent event) throws IOException {
+        FXMLLoader loader= new FXMLLoader();
+        loader.setLocation(getClass().getResource("/view/TransaksiPemasukan.fxml"));
+        loader.load();
+        AnchorPane root=loader.getRoot();
+        sp_pane_holder.getChildren().clear();
+        sp_pane_holder.getChildren().add(root);
     }
 
     @FXML
-    private void transaksi(ActionEvent event) {
-        pane_home.setVisible(false);
-        pane_transaksi.setVisible(true);
-        pane_dataWarga.setVisible(false);
+    private void transaksiPengeluaran(ActionEvent event) throws IOException {
+        FXMLLoader loader= new FXMLLoader();
+        loader.setLocation(getClass().getResource("/view/TransaksiPengeluaran.fxml"));
+        loader.load();
+        AnchorPane root=loader.getRoot();
+        sp_pane_holder.getChildren().clear();
+        sp_pane_holder.getChildren().add(root);
     }
 
     @FXML
-    private void laporan(ActionEvent event) {
+    private void buatLaporan(ActionEvent event) throws IOException {
+        FXMLLoader loader= new FXMLLoader();
+        loader.setLocation(getClass().getResource("/view/Cetak.fxml"));
+        loader.load();
+        AnchorPane root=loader.getRoot();
+        sp_pane_holder.getChildren().clear();
+        sp_pane_holder.getChildren().add(root);
+        
     }
 
     @FXML
-    private void simpanUser(ActionEvent event) {
+    private void mengatur_iuran(ActionEvent event) {
     }
 
     @FXML
-    private void resetAll(ActionEvent event) {
+    private void open_website(ActionEvent event) {
     }
 
-    @FXML
-    private void editUser(ActionEvent event) {
-    }
 
-    @FXML
-    private void hapusUser(ActionEvent event) {
-    }
-
-    @FXML
-    private void addUser(ActionEvent event) {
-    }
-
-    @FXML
-    private void call_pengeluaran(ActionEvent event) {
-        anchorPane_pengeluaran.setVisible(true);
-        anchorPane_Pemasukan.setVisible(false);
-    }
-
-    @FXML
-    private void call_pemasukan(ActionEvent event) {
-        anchorPane_pengeluaran.setVisible(false);
-        anchorPane_Pemasukan.setVisible(true);
-    }
-    
 }
